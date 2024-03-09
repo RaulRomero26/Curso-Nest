c9bf454f69e38aad0ee04b13610a957668aec166
Raul Romero <link_2642@hotmail.com>
2024-03-09 17:53:51 -0600

    update: peticion http

--- 01-typescript-intro/src/bases/03-classes.ts
+++ 01-typescript-intro/src/bases/03-classes.ts
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
 
 export class Pokemon {
     /*
@@ -12,10 +12,38 @@ export class Pokemon {
     }*/
 
     constructor(
-        public id: number,
-        public name: string
+        public readonly id: number,
+        public name: string,
+        //public imageUrl: string,
     ){}
 
+    get imageUrl(): string{
+        return `https://pokemon.com/${this.id}.jpg`
+    }
+
+    scream() {
+        console.log(`${this.name.toLocaleUpperCase()}!!!`);
+    }
+
+    speak() {
+        console.log(`${this.name}, ${this.name}`);
+    }
+
+    async getMoves(){
+        
+        //const moves =10;
+        
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+
+       console.log(data.moves);
+
+       return data.moves;
+    }
 }
 
 export const charmander = new Pokemon(4,'Charmander');
+
+console.log(charmander);
+charmander.speak();
+charmander.scream();
+charmander.getMoves();
