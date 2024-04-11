ab126dc66f7ca3cb27230cf7c18d2f79ca25ec6c
RaulRomero26 <link_2642@hotmail.com>
2024-04-11 17:45:42 -0600

    update: nest mongoose, docker compose, modelos

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -8,6 +10,10 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27019/nest-pokemon'),
+
+    PokemonModule,
   ],
 })
 export class AppModule {}
