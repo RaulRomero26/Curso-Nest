bcadf7b795afe55aa321364d97816bb780631ec8
RaulRomero26 <link_2642@hotmail.com>
2024-07-29 17:51:42 -0600

    update: app.config creado

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,19 +1,24 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/app.config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27019/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
